2ee9c5a6789bb4e006167ee3a156a91313302d64
Fcmam5 <dr.fcmam5@gmail.com>
2019-06-26 01:21:38 +0200

    translate components

--- src/components/ErrorDecoder/ErrorDecoder.js
+++ src/components/ErrorDecoder/ErrorDecoder.js
@@ -69,8 +69,8 @@ function ErrorResult(props: {|code: ?string, msg: string|}) {
   if (!code) {
     return (
       <p>
-        When you encounter an error, you'll receive a link to this page for that
-        specific error and we'll show you the full error text.
+        عندما تُواجه خطأ ما، سيتمّ تحويلك إلى هذه الصفحة الخاصة بذلك الخطأ بالتحديد 
+        وسنعرض لك نص رسالة الخطأ بالكامل.
       </p>
     );
   }
@@ -78,7 +78,7 @@ function ErrorResult(props: {|code: ?string, msg: string|}) {
   return (
     <div>
       <p>
-        <b>The full text of the error you just encountered is:</b>
+        <b>النص الكامل للخطأ الذي واجهته للتو هو:</b>
       </p>
       <code>
         <b>{urlify(errorMsg)}</b>
