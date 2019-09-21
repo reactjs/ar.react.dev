1a3cf86daa0de37ad207286eb9dc7399046fd114
imed jaberi <imed_jebari@hotmail.fr>
2019-09-21 15:55:52 +0200

    done translate blog/all.js content to ar ..

--- src/pages/blog/all.html.js
+++ src/pages/blog/all.html.js
@@ -28,10 +28,10 @@ const AllBlogPosts = ({data, location}: Props) => (
     <Container>
       <div css={sharedStyles.articleLayout.container}>
         <div css={sharedStyles.articleLayout.content}>
-          <Header>All Posts</Header>
+          <Header>جميع المشاركات</Header>
           <TitleAndMetaTags
             canonicalUrl={`${urlRoot}/blog/all.html`}
-            title="React - All Posts"
+            title="React - جميع المشاركات"
           />
           <ul
             css={{
