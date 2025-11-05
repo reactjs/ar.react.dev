1fb342b234268ca21eeab521e043733c93ddf556
Mohamed Hashem <hamo.hashem2019@gmail.com>
2025-11-05 22:14:50 +0200

    Fix TypeScript errors and add missing dependencies
    - Fixed JSX namespace errors in Icon components (React 19 compatibility)
    
    - Added @types/prop-types dependency
    
    - Added React Compiler dependencies to eslint-local-rules
    
    - Fixed all Icon components to use React.JSX.IntrinsicElements
    
    - Added proper TypeScript types to all Icon component props

--- src/components/Icon/IconPitfall.tsx
+++ src/components/Icon/IconPitfall.tsx
@@ -9,10 +9,10 @@
  * Copyright (c) Facebook, Inc. and its affiliates.
  */
 
-import {memo} from 'react';
+import React, {memo} from 'react';
 
-export const IconPitfall = memo<JSX.IntrinsicElements['svg']>(
-  function IconPitfall({className}) {
+export const IconPitfall = memo<React.JSX.IntrinsicElements['svg']>(
+  function IconPitfall({className}: {className?: string}) {
     return (
       <svg
         className={className}
